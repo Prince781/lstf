a22fc776175ed1bc5cd4b718cfd09c4586964603
Princeton Ferro <princetonferro@gmail.com>
2023-09-26 22:21:13 -0400

    implement interactive debugger
    Use -break to start the debugger. From there are a handful of basic
    single-letter commands.

--- src/vm/lstf-vm-value.c
+++ src/vm/lstf-vm-value.c
@@ -1,9 +1,52 @@
 #include "lstf-vm-value.h"
 #include "lstf-vm-coroutine.h"
+#include "lstf-vm-program.h"
 #include <assert.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+void lstf_vm_value_print(lstf_vm_value *value, lstf_vm_program *program,
+                         outputstream *ostream) {
+  switch (value->value_type) {
+  case lstf_vm_value_type_array_ref:
+  case lstf_vm_value_type_object_ref:
+  case lstf_vm_value_type_pattern_ref: {
+    char *json_representation =
+        json_node_to_string(value->data.json_node_ref, true);
+    outputstream_printf(ostream, "%s\n", json_representation);
+    free(json_representation);
+  } break;
+  case lstf_vm_value_type_boolean:
+    outputstream_printf(ostream, "%s\n", value->data.boolean ? "true" : "false");
+    break;
+  case lstf_vm_value_type_code_address:
+    outputstream_printf(ostream, "[VM code @ 0x%p]\n",
+                        (void *)value->data.address);
+    break;
+  case lstf_vm_value_type_double:
+    outputstream_printf(ostream, "%lf\n", value->data.double_value);
+    break;
+  case lstf_vm_value_type_integer:
+    outputstream_printf(ostream, "%" PRIi64 "\n", value->data.integer);
+    break;
+  case lstf_vm_value_type_null:
+    outputstream_printf(ostream, "null\n");
+    break;
+  case lstf_vm_value_type_string:
+    outputstream_printf(ostream, "%s\n", value->data.string->buffer);
+    break;
+  case lstf_vm_value_type_closure:
+    outputstream_printf(
+        ostream, "[closure [VM code @ %#" PRIxPTR "] [%u up-values]]",
+        (uintptr_t)(value->data.closure->code_address - program->code),
+        value->data.closure->num_upvalues);
+    break;
+  default:
+    fprintf(stderr, "%s: unexpected value type ID `%u'\n", __func__, value->value_type);
+    abort();
+  }
+}
 
 lstf_vm_upvalue *lstf_vm_upvalue_new(int64_t stack_offset, lstf_vm_coroutine *cr)
 {
