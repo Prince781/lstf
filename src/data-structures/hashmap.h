60f2b4a594e36211b7572a71cc42bd07493ccb83
Princeton Ferro <princetonferro@gmail.com>
2025-02-26 01:11:10 -0800

    more work on type-safe hashmap and list
    would require equivalence of anonymous struct types

--- src/data-structures/hashmap.h
+++ src/data-structures/hashmap.h
@@ -1,57 +1,47 @@
 #pragma once
 
 #include "array.h"
+#include "list.h"
 #include <stdbool.h>
-
-#define list_node(T)                                                           \
-  struct {                                                                     \
-    T data;                                                                    \
-    T *next;                                                                   \
-  }
-
-#define list(T)                                                                \
-  struct {                                                                     \
-    list_node(T) *head;                                                        \
-    size_t length;                                                             \
-  }
 
 // requires C23
 #define list_foreach(ls, elem, statements)                                     \
-  for (auto p = (ls)->head; p; p = (typeof(p))p->next) {                       \
-    auto elem = &p->data;                                                      \
-    statements;                                                                \
-  }
+    for (auto p = (ls)->head; p; p = (typeof(p))p->next) {                     \
+        auto elem = &p->data;                                                  \
+        statements;                                                            \
+    }
 
 #define hashmap_entry(keyT, valT)                                              \
-  struct {                                                                     \
-    keyT key;                                                                  \
-    valT val;                                                                  \
-  }
+    struct {                                                                   \
+        keyT key;                                                              \
+        valT val;                                                              \
+    }
 
 #define hashmap(keyT, valT)                                                    \
-  struct {                                                                     \
-    array(list(hashmap_entry(keyT, valT))) buckets;                            \
-    list(hashmap_entry(keyT, valT)) entries;                                   \
-    unsigned (*key_hash_func)(keyT);                                           \
-    bool (*key_equal_func)(const keyT, const keyT);                            \
-    keyT (*key_ref_func)(keyT);                                                \
-    void (*key_unref_func)(keyT);                                              \
-    valT (*val_ref_func)(valT);                                                \
-    void (*val_unref_func)(valT);                                              \
-  }
+    struct {                                                                   \
+        array(list(hashmap_entry(keyT, valT))) buckets;                        \
+        list(hashmap_entry(keyT, valT)) entries;                               \
+        unsigned (*key_hash_func)(keyT);                                       \
+        bool (*key_equal_func)(const keyT, const keyT);                        \
+        keyT (*key_ref_func)(keyT);                                            \
+        void (*key_unref_func)(keyT);                                          \
+        valT (*val_ref_func)(valT);                                            \
+        void (*val_unref_func)(valT);                                          \
+    }
 
 #define hashmap_init(hashmap, key_hash_fn, key_equal_fn, key_ref_fn,           \
                      key_unref_fn, val_ref_fn, val_unref_fn)                   \
-  do {                                                                         \
-    array_init(&(hashmap)->buckets);                                           \
-    assert(key_hash_fn && "key hash function is required!");                   \
-    (hashmap)->key_hash_func = key_hash_fn;                                    \
-    (hashmap)->key_equal_func = key_equal_fn;                                  \
-    (hashmap)->key_ref_func = key_ref_fn;                                      \
-    (hashmap)->key_unref_func = key_unref_fn;                                  \
-    (hashmap)->val_ref_func = val_ref_fn;                                      \
-    (hashmap)->val_unref_func = val_unref_fn;                                  \
-  } while (0)
+    do {                                                                       \
+        auto map = (hashmap);                                                  \
+        array_init(&map->buckets);                                             \
+        assert(key_hash_fn && "key hash function is required!");               \
+        map->key_hash_func  = key_hash_fn;                                     \
+        map->key_equal_func = key_equal_fn;                                    \
+        map->key_ref_func   = key_ref_fn;                                      \
+        map->key_unref_func = key_unref_fn;                                    \
+        map->val_ref_func   = val_ref_fn;                                      \
+        map->val_unref_func = val_unref_fn;                                    \
+    } while (0)
 
 // TODO:
 #define hashmap_resize_to_length(hashmap, new_length) \
@@ -63,40 +53,42 @@
 // 2. insert into the bucket
 // 3. if the
 #define hashmap_insert(hashmap, key_expr, val_expr)                            \
-  do {                                                                         \
-    unsigned hash = (hashmap)->key_hash_func(key_expr);                        \
-    hash %= (hashmap)->buckets.length;                                         \
+    do {                                                                       \
+        auto     map  = (hashmap);                                             \
+        unsigned hash = map->key_hash_func(key_expr) % map->buckets.length;    \
                                                                                \
-    if ((hashmap)->num_elements > 0.75f * (hashmap)->buckets.length)           \
-      hashmap_resize_to_length(hashmap, (hashmap)->buckets.length * 2);        \
+        if (map->entries.length > 0.75f * map->buckets.length)                 \
+            hashmap_resize_to_length(map, map->buckets.length * 2);            \
                                                                                \
-    bool inserted = false;                                                     \
-    list_foreach(&(hashmap)->buckets.elements[hash], entry, {                  \
-      if ((hashmap)->key_equal_func(entry->key, key_expr)) {                   \
-        if ((hashmap)->key_unref_func) {                                       \
-          (hashmap)->key_unref_func(entry->key);                               \
-          entry->key = (hashmap)->key_ref_func(key_expr);                      \
-        } else {                                                               \
-          entry->key = key_expr;                                               \
+        bool inserted = false;                                                 \
+        list_foreach(&map->buckets.elements[hash], entry, {                    \
+            if (map->key_equal_func(entry->key, key_expr)) {                   \
+                if (map->key_unref_func) {                                     \
+                    map->key_unref_func(entry->key);                           \
+                    entry->key = map->key_ref_func(key_expr);                  \
+                } else {                                                       \
+                    entry->key = key_expr;                                     \
+                }                                                              \
+                if (map->val_unref_func) {                                     \
+                    map->val_unref_func(entry->val);                           \
+                    entry->val = map->val_ref_func(val_expr);                  \
+                } else {                                                       \
+                    entry->val = val_expr;                                     \
+                }                                                              \
+                inserted = true;                                               \
+            }                                                                  \
+        });                                                                    \
+                                                                               \
+        if (!inserted) {                                                       \
+            typeof(map->buckets.elements[hash].tail->data) key_val = {         \
+                key_expr, val_expr};                                           \
+            list_append(&map->buckets.elements[hash], key_val);                \
+            inserted = true;                                                   \
         }                                                                      \
-        if ((hashmap)->val_unref_func) {                                       \
-          (hashmap)->val_unref_func(entry->val);                               \
-          entry->val = (hashmap)->val_ref_func(val_expr);                      \
-        } else {                                                               \
-          entry->val = val_expr;                                               \
-        }                                                                      \
-        inserted = true;                                                       \
-      }                                                                        \
-    });                                                                        \
-                                                                               \
-    if (!inserted) {                                                           \
-      list_append(&(hashmap)->buckets[hash], {key_expr, val_expr});            \
-      inserted = true;                                                         \
-    }                                                                          \
-  } while (0)
+    } while (0)
 
 static inline void test(void) {
-  hashmap(char *, double) map;
-  hashmap_init(&map, NULL, NULL, NULL, NULL, NULL, NULL);
-  hashmap_insert(&map, "hello", 3.4);
+    hashmap(char *, double) hmap;
+    hashmap_init(&hmap, NULL, NULL, NULL, NULL, NULL, NULL);
+    hashmap_insert(&hmap, "hello", 3.4);
 }
--- src/data-structures/list.h
+++ src/data-structures/list.h
@@ -4,43 +4,60 @@
 #include <stdlib.h>
 
 #define list_node(T)                                                           \
-  struct {                                                                     \
-    T data;                                                                    \
-    T *prev; /* cast to list_node(T), since [data] is at start of struct */    \
-    T *next; /* (same thing) */                                                \
-  }
+    struct {                                                                   \
+        T  data;                                                               \
+        T *prev; /* cast to list_node(T), since [data] is at start */          \
+        T *next; /* (same thing) */                                            \
+    }
 
 #define list(T)                                                                \
-  struct {                                                                     \
-    list_node(T) *head;                                                        \
-    list_node(T) *tail;                                                        \
-    unsigned length;                                                           \
-    T (*data_ref_func)(T);                                                     \
-    void (*data_unref_func)(T);                                                \
-  }
+    struct {                                                                   \
+        list_node(T) * head;                                                   \
+        list_node(T) * tail;                                                   \
+        unsigned length;                                                       \
+        T (*data_ref_func)(T);                                                 \
+        void (*data_unref_func)(T);                                            \
+    }
 
 /**
  * Append a new element [elem] to the list.
  */
 #define list_append(ls, elem)                                                  \
-  do {                                                                         \
-    typeof(ls) list = ls;                                                      \
-    if (!list->head) {                                                         \
-      assert(!list->tail && "list tail must be null when head is null");       \
-      list->head = (void *)(list->tail = calloc(1, sizeof *list->head));       \
-      list->head->data =                                                       \
-          list->data_ref_func ? list->data_ref_func(elem) : (elem);            \
-    } else {                                                                   \
-      typeof(list->tail) node = calloc(1, sizeof *list->tail);                 \
-      node->data = list->data_ref_func ? list->data_ref_func(elem) : (elem);   \
-      list->tail->next = (void *)node;                                         \
-      node->prev = (void *)list->tail;                                         \
-      list->tail = node;                                                       \
-    }                                                                          \
-  } while (0)
+    do {                                                                       \
+        auto list = (ls);                                                      \
+        if (!list->head) {                                                     \
+            assert(!list->tail && "list tail must be null when head is null"); \
+            list->head = (void *)(list->tail = calloc(1, sizeof *list->head)); \
+            list->head->data =                                                 \
+                list->data_ref_func ? list->data_ref_func(elem) : (elem);      \
+        } else {                                                               \
+            typeof(list->tail) node = calloc(1, sizeof *list->tail);           \
+            node->data =                                                       \
+                list->data_ref_func ? list->data_ref_func(elem) : (elem);      \
+            list->tail->next = (void *)node;                                   \
+            node->prev       = (void *)list->tail;                             \
+            list->tail       = node;                                           \
+        }                                                                      \
+    } while (0)
 
-static void x(void) {
-  list(int) l;
-  list_append(&l, 3);
-  list_append(&l, 4);
-}
+#define list_foreach(ls, elem, statements)                                     \
+    for (auto p = (ls)->head; p; p = (typeof(p))p->next) {                     \
+        auto elem = &p->data;                                                  \
+        statements;                                                            \
+    }
+
+
+#define list_pop(ls)                                                           \
+    do {                                                                       \
+        auto list = (ls);                                                      \
+        assert(list->tail && "popping an empty list");                         \
+        if (list->head == (void *)list->tail) {                                \
+            free(list->tail);                                                  \
+            list->head = (void *)(list->tail = nullptr);                       \
+        } else {                                                               \
+            auto old_tail    = list->tail;                                     \
+            list->tail       = (void *)list->tail->prev;                       \
+            list->tail->next = (void *)list->head;                             \
+            free(old_tail);                                                    \
+        }                                                                      \
+    } while (0)
